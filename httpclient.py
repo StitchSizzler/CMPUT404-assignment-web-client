6eb533e5e028e82d303ffdebd3aa43191125f1b8
StitchSizzler <eisha@ualberta.ca>
2022-02-11 10:39:35 -0700

    initial commit

--- httpclient.py
+++ httpclient.py
@@ -41,13 +41,21 @@ class HTTPClient(object):
         return None
 
     def get_code(self, data):
-        return None
+        try:
+            code = int(data.splitlines()[0].split()[1])
+        except:
+            code = None
+        return code
 
     def get_headers(self,data):
         return None
 
     def get_body(self, data):
-        return None
+        try: 
+            body = data.split("\r\n\r\n")[1]
+        except:
+            body = ""
+        return body
     
     def sendall(self, data):
         self.socket.sendall(data.encode('utf-8'))
@@ -67,14 +75,105 @@ class HTTPClient(object):
                 done = not part
         return buffer.decode('utf-8')
 
+    def get_host(self, urlParsed):
+        try:
+            if ":" in urlParsed.netloc:
+                return urlParsed.netloc.split(":")[0]
+            else:
+                return socket.gethostbyname(urlParsed.netloc)
+        except:
+            return None
+
+    def parse_url(self, url):
+        urlParsed = urllib.parse.urlparse(url)
+        urlParams = (url + "?").split("?")[1].split('#')
+        urlQuery = urlParams[0]
+        urlFragm = ""
+        if (len(urlParams) > 1):
+            urlFragm = urlParams[1]
+
+        urlDetails = {
+            "host": urlParsed.hostname,
+            "path": urlParsed.path,
+            "port": urlParsed.port,
+            "query": urlQuery,
+            "fragm": urlFragm
+        }
+
+        if urlDetails["path"] == "":
+            urlDetails["path"] = "/"
+        if not urlDetails["port"]:
+            urlDetails["port"] = 80
+
+        return urlDetails
+
     def GET(self, url, args=None):
         code = 500
         body = ""
+        request = ""
+        data = ""
+        urlDetails = self.parse_url(url)
+        host = urlDetails["host"]
+        path = urlDetails["path"]
+        port = urlDetails["port"]
+        query = urlDetails["query"]
+        fragm = urlDetails["fragm"]
+
+        if query:
+            path = path + "?" + query
+        if fragm:
+            path = path + "#" + fragm
+
+        request = "GET " + path + " HTTP/1.1\r\nHost: " + host + "\r\nUser-Agent: Mozilla/5.0\r\nConnection: close\r\n\r\n"
+        self.connect(host, port)
+        self.sendall(request)
+        data = self.recvall(self.socket)
+        self.close()
+        code = self.get_code(data)
+        body = self.get_body(data)
+
+        sys.stdout.write(body)
         return HTTPResponse(code, body)
 
     def POST(self, url, args=None):
         code = 500
         body = ""
+        request = ""
+        data = ""
+        urlDetails = self.parse_url(url)
+        host = urlDetails["host"]
+        path = urlDetails["path"]
+        port = urlDetails["port"]
+        query = urlDetails["query"]
+        fragm = urlDetails["fragm"]
+
+        if fragm:
+            path = path + "#" + fragm
+
+        if args:
+            for (key, value) in args.items():
+                body += key + "=" + value + "&"
+        if query:
+            queryList = urlDetails.split("&")
+            for item in queryList:
+                key = item.split("=")[0]
+                value = item.split("=")[1]
+                body += key + "=" + value + "&"
+        body = body[:-1]
+
+        request = "POST " + path + " HTTP/1.1\r\nHost: " + host + "\r\n"
+        request += "Content-Type: application/x-www-form-urlencoded\r\n"
+        request += "Content-Length: " + str(len(body)) + " \r\nUser-Agent: Mozilla/5.0\r\n"
+        request += "Connection: close\r\n\r\n" + body + "\r\n\r\n"
+
+        self.connect(host, port)
+        self.sendall(request)
+        data = self.recvall(self.socket)
+        self.close()
+        code = self.get_code(data)
+        body = self.get_body(data)
+
+        sys.stdout.write(body)
         return HTTPResponse(code, body)
 
     def command(self, url, command="GET", args=None):
